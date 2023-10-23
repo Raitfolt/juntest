0e4a6d0cf8c7e3206710587ba0c86410ecdb5226
Anton Novoseltsev <raitfolt@gmail.com>
2023-10-23 15:46:15 +0500

    still work with Storage funcs

--- cmd/juntest/main.go
+++ cmd/juntest/main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"github.com/Raitfolt/juntest/config"
 	"github.com/Raitfolt/juntest/internal/logger"
+	"github.com/Raitfolt/juntest/internal/storage/psql"
+	"go.uber.org/zap"
 )
 
 func main() {
+	//CONFIG_PATH
 	cfg := config.MustLoad()
 
 	log := logger.New(cfg.LogPath)
+	log.Info("logger loaded")
 	defer log.Sync()
 
-	log.Info("Logger loaded")
+	db, err := psql.New(log, cfg)
+	if err != nil {
+		log.Fatal("postgre connection", zap.String("error", err.Error()))
+	}
+	defer db.DB.Close()
+
+	log.Info("database loaded")
+
 }
--- internal/storage/psql/psql.go
+++ internal/storage/psql/psql.go
@@ -1,19 +1,67 @@
 package psql
 
 import (
+	"database/sql"
 	"fmt"
 
 	"github.com/Raitfolt/juntest/config"
+	_ "github.com/lib/pq"
 	"go.uber.org/zap"
 )
 
 type Storage struct {
-	//db *sql.DB
+	DB *sql.DB
 }
 
-func New(log *zap.Logger, cfg *config.Config) {
+func New(log *zap.Logger, cfg *config.Config) (*Storage, error) {
 	log.Info("create new storage connect")
-	psqlInfo := fmt.Sprintf("host=%s port =%d user=%s password=%s sslmode=disalbe",
+	psqlInfo := fmt.Sprintf("host=%s port =%d user=%s password=%s sslmode=disable",
 		cfg.PostgresHost, cfg.PostgresPort, cfg.PostgresUser, cfg.PostgresPassword)
 	log.Info("postgresql", zap.String("connection string", psqlInfo))
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Error("sql.Open", zap.String("error", err.Error()))
+		return nil, err
+	}
+
+	_, err = db.Exec("DROP DATABASE " + cfg.PostgresDB)
+	if err != nil {
+		log.Error("clear database", zap.String("error", err.Error()))
+		return nil, err
+	}
+	log.Info("database cleared")
+
+	_, err = db.Exec("CREATE DATABASE " + cfg.PostgresDB)
+	if err != nil {
+		log.Error("create database", zap.String("error", err.Error()))
+		return nil, err
+	}
+	log.Info("database created")
+
+	_, err = db.Exec(`CREATE TABLE persons(
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(255),
+		surname VARCHAR(255),
+		patronymic VARCHAR(255),
+		age INTEGER,
+		gender VARCHAR(10),
+		nationality VARCHAR(255)
+	);`)
+	if err != nil {
+		log.Error("create table", zap.String("error", err.Error()))
+		return nil, err
+	}
+	log.Info("table persons created")
+
+	return &Storage{DB: db}, nil
 }
+
+//TODO: realize method for storage: NewPerson
+//TODO: realize method for storage: DeletePersonByID
+//TODO: realize method for storage: ChangePerson
+//TODO: realize method for storage:
+
+/*func (s *Storage) NewPerson(name, surname, patronymic string) (int64, error){
+	stmt, err := s.DB.Prepare("INSERT INTO ")
+}*/
