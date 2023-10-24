c60163584af52e9805802b142913eaae2b090574
Антон Новосельцев <raitfolt@gmail.com>
2023-10-24 08:05:19 +0500

    add request to outer services

--- internal/httpServer/handlers/add/add.go
+++ internal/httpServer/handlers/add/add.go
@@ -5,13 +5,17 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/go-resty/resty/v2"
 	"go.uber.org/zap"
 )
 
 type Person struct {
-	Name       string `json:"name" validate:"required"`
-	Surname    string `json:"surname" validate:"required"`
-	Patronymic string `json:"patronymic,omitempty"`
+	Name        string `json:"name" validate:"required"`
+	Surname     string `json:"surname" validate:"required"`
+	Patronymic  string `json:"patronymic,omitempty"`
+	Age         int    `json:"age,omitempty"`
+	Gender      string `json:"gender,omitempty"`
+	Nationality string `json:"nationality,omitempty"`
 }
 
 type PersonSaver interface {
@@ -27,12 +31,26 @@ func New(log *zap.Logger, personSaver PersonSaver) http.HandlerFunc {
 		data, err := io.ReadAll(r.Body)
 		if err != nil {
 			log.Error("read body from request", zap.String("error", err.Error()))
+			return
 		}
 
 		err = json.Unmarshal(data, &person)
 		if err != nil {
 			log.Error("unmarshall body", zap.String("error", err.Error()))
+			return
 		}
-		//TODO: need to complete
+		log.Info("body decoded", zap.String("name", person.Name), zap.String("surname", person.Surname))
+
+		var age int
+		resty.New().R().SetResult(&age).Get("https://api.agify.io/?name=" + person.Name)
+		log.Info("get age", zap.String("name", person.Name), zap.Int("age", person.Age))
+
+		var gender string
+		resty.New().R().SetResult(&gender).Get("https://api.genderize.io/?name=" + person.Name)
+		log.Info("get gender", zap.String("name", person.Name), zap.String("gender", person.Gender))
+
+		var nationality string
+		resty.New().R().SetResult(&nationality).Get("https://api.nationalize.io/?name=" + person.Name)
+		log.Info("get nationality", zap.String("name", person.Name), zap.String("age", person.Nationality))
 	}
 }
